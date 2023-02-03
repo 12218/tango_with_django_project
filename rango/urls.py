fe7b2c76b1c198f639bafd5de41a18f4f3e9ea59
12218 <2402767956@qq.com>
2023-02-03 10:04:07 +0000

    Week03 - Chapter6

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('index/', index),
-    path('about/', about)
+    path('about/', about),
+    path('category/<slug:category_name_slug>', show_category, name = 'category')
 ]
